60f5fe374b503671e49630ecc3c04681a63522e6
zhangjie <zhangjie@taoshouyou.com>
2017-12-21 18:12:58 +0800

    index控制器和models创建成功

--- js.e01/modules/edbug/controllers/IndexController.php
+++ js.e01/modules/edbug/controllers/IndexController.php
@@ -3,14 +3,21 @@ namespace app\modules\edbug\controllers;
 
 use Yii;
 use yii\base\Controller;
+use app\modules\edbug\models\Porcess;
+use app\modules\edbug\models\Parse;
 
 
 class IndexController extends Controller
 {
-
-
-    function actionIndex(){
-        echo __FUNCTION__;
+    public function actionIndex(){
+        global $oper;
+        //处理传入的参数
+        Porcess::request();     
+        $main = ucfirst($oper); 
+        $parseMethod = new Parse();
+        if(method_exists($parseMethod,'get'.$main)){
+            $parseMethod->{'get'.$main}();
+        }
     }
 }
 
