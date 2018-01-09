7e039111668c7eb6a09bc415268a57855b83fe4a
HD-010 <912744609@qq.com>
2018-01-09 08:46:02 +0800

    添加shema

--- dataAnalysis/modules/analysis/Module.php
+++ dataAnalysis/modules/analysis/Module.php
@@ -10,7 +10,6 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
-
         // custom initialization code goes here
     }
 }
--- dataAnalysis/modules/analysis/controllers/IndexController.php
+++ dataAnalysis/modules/analysis/controllers/IndexController.php
@@ -3,11 +3,14 @@ namespace app\modules\analysis\controllers;
 
 use Yii;
 use yii\base\Controller;
+use app\modules\analysis\models\Schema;
 
 class IndexController extends Controller
 {
     public function actionIndex(){
-        
+        $schema = new Schema();
+        $schema->setTableDesc('user');
+        echo "<pre>";print_r(Schema::$userDesc);echo "</pre>";
     }
 }
 
