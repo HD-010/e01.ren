0738ce6c4b2e57fc0ce6096209bf0dc18f754d56
HD-010 <912744609@qq.com>
2018-01-20 08:18:37 +0800

    dubug

--- frontend/components/ebug/EbugTranceData.php
+++ frontend/components/ebug/EbugTranceData.php
@@ -7,18 +7,15 @@ class EbugTranceData
         $ch = curl_init();
         curl_setopt($ch,CURLOPT_URL,EBUG_SERVER);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        
+        curl_setopt($ch, CURLOPT_HEADER, 0);
         // post数据
         curl_setopt($ch, CURLOPT_POST, 1);
-        
         //执行的最长秒数
         curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-        
         // post的变量
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         $output = curl_exec($ch);
         curl_close($ch);
-        
         //打印获得的数据
         return $output;
     }
