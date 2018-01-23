b97d5c36566a012125c49efdc59ba28b6cc924f2
HD-010 <912744609@qq.com>
2018-01-23 08:37:52 +0800

    dubug

---
+++ dataAnalysis/components/views/header/normal.php
@@ -0,0 +1,17 @@
+<div name=top>
+	<div name=log>eBug</div>
+	<ul name=state>
+		<li name=nickName></li>
+	</ul>
+	<ul name=func>
+		<li name=t1><a href="">关于</a>
+			<ul name=t2>
+				<li><a href="javascript:" name="setSys">个性设置</a></li>
+				<li><a href="javascript:" name="journal">清空日志</a></li>
+				<li><a href="javascript:" name="login|out">登录</a></li>
+			</ul>
+		</li>
+	</ul>
+</div>
+<div class=space></div>
+<script data-main="./assets/js/app/header_normal" src="./assets/js/require.js"></script>
