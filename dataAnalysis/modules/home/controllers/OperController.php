bde555f584093f44713e863bf506721661fdfb32
zhangjie <zhangjie@taoshouyou.com>
2018-01-26 10:22:40 +0800

    修改事件选项面板数据加载

--- dataAnalysis/modules/home/controllers/OperController.php
+++ dataAnalysis/modules/home/controllers/OperController.php
@@ -14,23 +14,6 @@ class OperController extends Controller
 {
     
     /**
-     * 获取事件分析选项中显示事件名称项内容
-     */
-    public function actionName(){
-        //获取所有事件名称
-        $names = (new DataProcess())->getAllEventNames();
-        T::outJson($names);
-    }
-    
-    /**
-     * 获取事件分析选项中显示事件属性项内容
-     */
-    public function actionAttr(){
-        //获取所有事件名称
-        $names = (new DataProcess())->getAllEventAttr();
-        T::outJson($names);
-    }
-    /**
      * 获取事件分析选项中显示事件属性项内容
      */
     public function actionEventOpt(){
