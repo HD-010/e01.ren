62301280dced9aa8404148454e2d7d0284958151
zhangjie <zhangjie@taoshouyou.com>
2018-02-10 09:55:27 +0800

    test

--- dataAnalysis/modules/home/models/DataProcess.php
+++ dataAnalysis/modules/home/models/DataProcess.php
@@ -20,8 +20,8 @@ class DataProcess
         //验证数据库连接是否有效，如果无效则创建新的连接
         $conn = \Yii::$app->db;
         $sql = "select count(*) from e01ren_development_events";
-        $command = $conn->createCommand($sql)->queryAll();
-        return $command;
+        $res = $conn->createCommand($sql)->queryAll();
+        return $res;
         //查询数据
     }
     
